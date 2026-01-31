1d1eab1bfba89244e86691b3c1901940fa6b90a1
unknown <riditjangra09@gmail.com>
2026-01-31 23:51:13 +0530

    chore: add remote (but locally) access and push and clone commands

--- forge.py
+++ forge.py
@@ -2,9 +2,12 @@ import os
 import sys
 import json
 import datetime
+import requests
+from pathlib import Path
 
 folder_name = ".forge"
 folder_path = f"./{folder_name}"
+server_url = "http://127.0.0.1:7420"
 
 def get_new_commit_id() -> str:
     return datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
@@ -16,8 +19,9 @@ def init_repo() -> None:
     
     os.mkdir(folder_path)
 
-    json_files = ["files.json", "commits.json"]
+    json_files = ["files.json", "commits.json", "repo.json"]
     txt_files = ["current_commit.txt"]
+    repo_name = Path.cwd().name
     folders = []
 
     try:
@@ -36,18 +40,40 @@ def init_repo() -> None:
 
         init_commit_id = get_new_commit_id()
 
+        commits_data = [{"id": init_commit_id, "message": "Init commit.", "files": []}]
+        
         with open(f"./{folder_path}/commits.json", "w") as f:
-            commits = [{"id": init_commit_id, "message": "Init commit.", "files": []}]
-            f.write(json.dumps(commits))
+            f.write(json.dumps(commits_data))
         
         with open(f"./{folder_path}/current_commit.txt", "w") as f:
             f.write(init_commit_id)
 
+        repo_data = {"name": repo_name, "path":  f"{Path.cwd()}"}
+        request_repo_data = {"name": repo_name, "path":  f"{Path.cwd()}", "commits": commits_data, "files": []}
+        
+        with open(f"./{folder_path}/repo.json", "w") as f:
+            f.write(json.dumps(repo_data))
+
+        request = requests.post(f"{server_url}/repos/init", json=request_repo_data)
+        print(f"[Remote] {request.json()}")
+
+        push_data = {"repo_name": get_repo_name(), "commit_id": init_commit_id}
+        request = requests.post(f"{server_url}/repos/push", json=push_data)
+    
+        print(f"[Remote] (Push Changes) {request.json()}")
+
     except Exception as e:
-        print(f"Error occured: ${e}")
+        print(f"Error occured: {e}")
         return
 
     print("A new start!!!!")
+
+def get_repo_name() -> str:
+    name = ""
+    with open(f"./{folder_path}/repo.json", "r") as f:
+        data = json.loads(f.read())
+        name = data["name"]
+    return name    
 
 def load_file_data() -> list:
     data = []
@@ -84,10 +110,8 @@ def add_file(file: dict) -> None:
     file_path = file.get("path")
     content = file.get("content")
 
-    # remove any previous entry for this file
     files_entries = [f for f in files_entries if f.get("file_path") != file_path]
 
-    # determine status
     status = "new"
     if os.path.exists(file_path):
         try:
@@ -107,6 +131,7 @@ def add_file(file: dict) -> None:
     try:
         with open(f"{folder_path}/files.json", "w") as f:
             f.write(json.dumps(files_entries))
+        
     except Exception as e:
         print(f"Error occured: {e}")
 
@@ -187,7 +212,7 @@ def add_commit(message: str) -> None:
 
     commit_id = get_new_commit_id()
     file_entries = load_file_data()
-    
+
     add_commit_entry({"id": commit_id, "message": message, "files": file_entries})
     set_current_commit(commit_id)
 
@@ -262,6 +287,79 @@ def print_commit_data() -> None:
             print(f"File ({file.get("status")}): {file.get("file_path")}")
         print("\n")
 
+def push_changes() -> None:
+    commit = get_commit(get_current_commit())
+    commit_data = {"id": commit.get("id"), "message": commit.get("message"), "files": commit.get("files"), "repo_name": get_repo_name()}
+    request = requests.post(f"{server_url}/repos/commits/add", json=commit_data)
+    print(f"[Remote] (Commit Changes) {request.json()}")
+
+    files_entries = load_file_data()
+
+    reset_files_data = {"repo_name": get_repo_name()}
+    request = requests.post(f"{server_url}/repos/files/reset", json=reset_files_data)
+    print(f"[Remote] (File Reset) {request.json()}")
+
+    for file_entry in files_entries:
+        file_data = {"path": file_entry.get("file_path"), "content": file_entry.get("file_content"), "repo_name": get_repo_name(), "status": file_entry.get("status")}
+        request = requests.post(f"{server_url}/repos/files/add", json=file_data)
+        print(f"[Remote] (File Changes) {request.json()}")
+
+    push_data = {"repo_name": get_repo_name(), "commit_id": commit.get("id")}
+    request = requests.post(f"{server_url}/repos/push", json=push_data)
+    
+    print(f"[Remote] (Push Changes) {request.json()}")
+
+    print(f"[Local] (Push Complete) Done")
+
+def clone_repo(repo_name: str, dir_path: str = "") -> None:
+    clone_data = {"repo_name": repo_name}
+    response = requests.post(f"{server_url}/repos/clone", json=clone_data)
+
+    if response.status_code != 200:
+        print(f"[Remote] Error: {response.json().get('detail', 'Repo not found')}")
+        return
+
+    data = response.json()
+    if data.get("status") != "ok":
+        print(f"[Remote] Error: {data.get('message', 'Repo not found')}")
+        return
+
+    print(f"[Remote] (Found Repo) {repo_name}")
+    print(f"[Remote] (Starting Cloning) Starting...")
+
+    data = response.json()
+    files = data["files"]
+    commits = data["commits"]
+
+    if dir_path != "":
+        repo_name = dir_path
+
+    try:
+        os.mkdir(repo_name)
+        os.mkdir(f"{repo_name}/.forge")
+
+        with open(f"{repo_name}/.forge/files.json", "w") as f:
+            f.write(json.dumps(files))
+        
+        with open(f"{repo_name}/.forge/commits.json", "w") as f:
+            f.write(json.dumps(commits))
+
+        for file in files:
+            file_path = Path(f"{repo_name}") / file.get("file_path")
+
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+
+            content = file.get("content") or ""
+            file_path.write_text(content)
+
+            print(f"[Remote] (Fetching File) {file.get("file_path")}...")
+
+
+        print(f"[Local] (Completed Cloning) Completed...")
+        print(f"[Local] cd {repo_name} to continue.")
+    except Exception as e:
+        print(f"Error ocurred: {e}")
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Use a valid command: init")
@@ -318,10 +416,24 @@ if __name__ == "__main__":
         if list_id == "commits":
             print_commit_data()
         else:
-            print("Add a thing to list eg: commits.")
+            print("Add a parameter to list eg: commits.")
             quit()
+    elif command == "push":
+        push_changes()
+    elif command == "clone":
+        if len(sys.argv) < 3:
+            print("Add a repo name to clone.")
+            quit()
+        repo_name = sys.argv[2]
+        if len(sys.argv) < 4:
+            print("No path given defaulting to repo name.")
+        else:
+            dir_path = sys.argv[3]
+            clone_repo(repo_name, dir_path)
+            quit()
+        clone_repo(repo_name)
     elif command == "help":
-        print("All commands: init, list, add, remove")
+        print("All commands: init, list, add, remove, push")
     else:
-        print("Use a valid command: init, list, add, remove")
+        print("Use a valid command: init, list, add, remove, push")
         quit()
